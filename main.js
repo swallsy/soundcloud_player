5a5ca4bb459244ff439a07b54064258afd6bfa66
swallsy <smwall84@gmail.com>
2017-06-22 22:45:29 -0400

    updates to functionality and styling.

--- main.js
+++ main.js
@@ -1,30 +1,47 @@
-let searchInput = document.querySelector("#search_query");
-let searchForm = document.querySelector(".search-form");
-let results = document.querySelector(".results");
-let searchSubmitButton = document.querySelector("#submit");
-let header = document.querySelector("header");
-let searchContainer = document.querySelector(".search");
-let footer = document.querySelector("footer");
+// TO DO//
+
+// 1. Add in functionality to make the play button change to pause and vice versa
+// 4. If time, add in additional animations
+
+const searchInput = document.querySelector("#search_query");
+const searchForm = document.querySelector(".search-form");
+const results = document.querySelector(".results");
+const searchSubmitButton = document.querySelector("#submit");
+const header = document.querySelector("header");
+const searchContainer = document.querySelector(".search");
+const footer = document.querySelector("footer");
+let playing;
+
 // SEARCH BY TRACK FROM USERNAME //
 searchForm.addEventListener('submit', searchMusic);
 
-function searchMusic (e) {
+
+
+function searchMusic(e) {
   'use strict';
+
   e.preventDefault();
 
-  header.style.display='none';
-  searchContainer.style.transition='5s';
+  header.classList.add('collapsed');
+  footer.classList.add('collapsed');
 
-  let searchQuery = searchInput.value;
-  fetch(`http://api.soundcloud.com/tracks/?q=${searchQuery}&client_id=8538a1744a7fdaa59981232897501e04`)
-    .then(
+  // searchContainer.style.transition='all 5s';
 
-      function(response) {
-        if (response.status !== 200) {
-          console.log('Looks like there was a problem. Status Code: ' + response.status);
-          return;
 
-        } else {
+  setTimeout(function() {
+    let searchQuery = searchInput.value;
+
+    fetch(`http://api.soundcloud.com/tracks/?q=${searchQuery}&client_id=8538a1744a7fdaa59981232897501e04`)
+      .then(
+
+
+        function(response) {
+          if (response.status !== 200) {
+            console.log('Looks like there was a problem. Status Code: ' + response.status);
+            return;
+
+          } else {
+
             response.json().then(function(data) {
               results.innerHTML = "";
               for (let i = 0; i < data.length; i++) {
@@ -32,21 +49,22 @@ function searchMusic (e) {
                 let title = data[i].title;
                 let url = data[i].permalink_url;
                 let likes = data[i].likes_count;
-                let artist = data[i].user.username;
+                let user = data[i].user.username;
                 let audioFile = data[i].stream_url;
+                let soundcloudLink = data[i].permalink_url;
 
                 let markup = `
                       <div class="track">
                         <div class="image-container">
                           <button id="playButton${i}" class="play" style="background-image: url(${artwork})">&#9654;</button>
                         </div>
-                        <div class="song_info">
-                          <h3>${artist}</h3>
-                          <p class="title">${title}</p>
-                          <div class="second_row">
-                            <p class="likes">Likes: ${likes}</p>
-                          </div>
-                        </div>
+                          <a href="${soundcloudLink}" class="song_info" target="_blank">
+                            <p class="user">${user}</p>
+                            <h3 class="title">${title}</h3>
+                            <div class="second_row">
+                              <p class="likes">${likes} likes</p>
+                            </div>
+                          </a>
                         <section class="player">
                           <audio id="audioFile${i}" class="music-player" controls="controls" src="${audioFile}/?client_id=8538a1744a7fdaa59981232897501e04"></audio>
                         </section>
@@ -54,18 +72,40 @@ function searchMusic (e) {
                   `
                 results.innerHTML += markup;
 
-             }
+              }
 
-             for (let j = 0; j < data.length; j++) {
-               let audioButton = document.querySelector(`#playButton${j}`);
-               console.log(audioButton);
-               let audioPlayer = document.querySelector(`#audioFile${j}`);
-               audioButton.addEventListener('click', function(ev) {
-                 audioPlayer.play();
-               }); //End event listener
-             } //End second for loop
-           }) // End response json
-        } // end
-      } // end function response
-    ) // end then
+              for (let j = 0; j < data.length; j++) {
+
+                let audioButton = document.querySelector(`#playButton${j}`);
+                let audioPlayer = document.querySelector(`#audioFile${j}`);
+
+                function updateButton() {
+                  let icon = this.paused ? "►" : "❚ ❚";
+                  audioButton.textContent = icon;
+                }
+
+                function playPauseAudio() {
+                  if (audioPlayer.paused) {
+                    if (playing) {
+                      playing.pause();
+                    }
+                    audioPlayer.play();
+                    playing = audioPlayer;
+                  } else {
+                    audioPlayer.pause();
+                    playing = false;
+                  }
+                }
+
+                audioPlayer.addEventListener('play', updateButton);
+                audioPlayer.addEventListener('pause', updateButton);
+                audioButton.addEventListener('click', playPauseAudio);
+
+
+              } //End second for loop
+            }) // End response json
+          } // end
+        } // end function response
+      ) // end then
+  }, 300);
 } //end search  function
